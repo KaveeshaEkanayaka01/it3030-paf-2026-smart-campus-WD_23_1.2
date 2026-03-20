dd1d0cb83b0b11e74e56bbd5ac662c5d20657c8a
Tharindu409 <ntharindu331@gmail.com>
2026-03-20 21:27:54 +0530

    just removed the unnecessary files

--- src/main/java/lk/sliit/it3030/smartcampus/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package lk.sliit.it3030.smartcampus.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
