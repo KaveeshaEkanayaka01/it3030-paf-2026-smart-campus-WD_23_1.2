1614d75ced5d175850704526c3ef3e7c12408937
Anupa Denil Amarasekara <anupadenil@gmail.com>
2026-04-14 20:47:24 +0530

    delete unnecessary files

--- src/test/java/lk/sliit/it3030/smartcampus/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package lk.sliit.it3030.smartcampus.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
