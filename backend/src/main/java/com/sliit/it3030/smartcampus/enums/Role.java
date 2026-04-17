f7d91ed200a7a1c951f07a2495c4b5813c7afd87
KaveeshaEkanayaka01 <kavee0106@gmail.com>
2026-04-17 22:54:36 +0530

    Cleaned project - Member 1 (Resources only)

--- backend/src/main/java/com/sliit/it3030/smartcampus/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package com.sliit.it3030.smartcampus.enums;
-
-public enum Role {
-    USER, ADMIN
-}
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,32 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import AdminLayout from "../layouts/AdminLayout";
-import LoginPage from "../pages/LoginPage";
 import DashboardPage from "../pages/DashboardPage";
 import ResourcesPage from "../pages/ResourcesPage";
 import AdminResourcesPage from "../pages/AdminResourcesPage";
 import NotFoundPage from "../pages/NotFoundPage";
-import OAuthSuccessPage from "../pages/OAuthSuccessPage";
-import ProtectedRoute from "../components/common/ProtectedRoute";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/oauth-success" element={<OAuthSuccessPage />} />
-
-        <Route element={<ProtectedRoute />}>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<DashboardPage />} />
-            <Route path="/resources" element={<ResourcesPage />} />
-          </Route>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/resources" element={<ResourcesPage />} />
         </Route>
 
-        <Route element={<ProtectedRoute allowedRoles={["ADMIN"]} />}>
-          <Route element={<AdminLayout />}>
-            <Route path="/admin/resources" element={<AdminResourcesPage />} />
-          </Route>
+        <Route element={<AdminLayout />}>
+          <Route path="/admin/resources" element={<AdminResourcesPage />} />
         </Route>
 
         <Route path="*" element={<NotFoundPage />} />
